cbf00e75ee7240070b6f502588ca5fd83e1cd4d6
AndroPyro28 <Menandroeugenio1028@gmail.com>
2022-04-28 22:30:04 +0800

    asdasdsa

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,7 @@ function App() {
               isAuth={Cookies.get("userToken")}
             />
 
-            <Route path="*">{() => window.location.assign("/login")}</Route>
+            {/* <Route path="*">{() => window.location.assign("/login")}</Route> */}
           </Switch>
 
           {currentUser && !chatboxData.userData && (
