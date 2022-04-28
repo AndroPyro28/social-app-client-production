ea9db767a8e4c8a4e7fa7699ac280614985ca8fc
AndroPyro28 <Menandroeugenio1028@gmail.com>
2022-04-28 22:14:29 +0800

    balik tayo sa dati

--- src/App.jsx
+++ src/App.jsx
@@ -143,7 +143,7 @@ function App() {
     !loading && (
       <div className="App">
         <CreateGlobalStyle />
-        <Router basename="https://andropyro28.github.io/social-app-client-production/">
+        <Router basename="/social-app-client-production/">
           {Cookies.get("userToken") ? <ProtectedNav /> : <UnProtectedNav />}
 
           <Switch>
