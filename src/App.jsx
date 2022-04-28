6cf2609b48e5bdfd004748bf3fae8b03ecc46d1f
AndroPyro28 <Menandroeugenio1028@gmail.com>
2022-04-28 22:10:58 +0800

    still tryinggg

--- src/App.jsx
+++ src/App.jsx
@@ -143,7 +143,7 @@ function App() {
     !loading && (
       <div className="App">
         <CreateGlobalStyle />
-        <Router basename="/social-app-client-production/userProfile/1">
+        <Router basename="https://andropyro28.github.io/social-app-client-production/">
           {Cookies.get("userToken") ? <ProtectedNav /> : <UnProtectedNav />}
 
           <Switch>
